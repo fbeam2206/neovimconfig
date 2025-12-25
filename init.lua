106b25acba56b4c5d641c34c9bac70f23d59ba34
graham <fgbeam1999@gmail.com>
2025-12-25 13:17:02 -0600

    toggle neotree

--- init.lua
+++ init.lua
@@ -13,7 +13,6 @@ vim.api.nvim_create_autocmd("ColorScheme", {
   callback = function()
     vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
     vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.api.nvim_set_hl(0, "LineNr", { bg = "NONE" })
     vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "NONE" })
     vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -80,7 +80,6 @@ return{
            pattern = "yugen",
            callback = function()
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
         vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
         vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none" })
